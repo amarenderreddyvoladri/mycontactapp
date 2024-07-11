b1191bd2490dccaedb3abbfa9d269b716a43c6db
amarenderreddyvoladri <amarender.voladri@gmail.com>
2024-07-11 16:37:39 +0530

    Update Hello.java

--- src/mycontactapp/Hello.java
+++ src/mycontactapp/Hello.java
@@ -3,5 +3,6 @@ package mycontactapp;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HellomEVeryBOdy iam from mycontactapp");
+		System.out.println("secondtime iam from mycontactapp");		
 	}
 }
